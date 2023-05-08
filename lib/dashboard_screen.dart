188e2cac7ab7fdb0cefd655b4fb9737f9a54ae09
Esen888 <esensharsheev808@gmail.com>
2023-05-08 14:11:53 +0600

    add_icon_button

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -1,11 +1,20 @@
+import 'package:firebase_flutter_both/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DashBoardScreen extends StatelessWidget {
   const DashBoardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Get.to(WelcomeScreen());
+            }),
+      ),
       body: Center(
         child: Text('Dashboard'),
       ),
